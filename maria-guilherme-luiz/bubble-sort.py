2bc41e14b2e37817406807436e77de6787f1bbc6
Guilherme Malagutti <guimalacastro.gm@gmail.com>
2015-09-25 10:20:57 -0300

    foi adicionado comandos para plotar plotar duas figuras uma com o range e a variavel cartas desordenadas e outra com cartas ordenadas

--- maria-guilherme-luiz/bubble-sort.py
+++ maria-guilherme-luiz/bubble-sort.py
@@ -1,5 +1,14 @@
+import matplotlib.pyplot as plt
 cartas = [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8] # lista das cartas
 print("Lista original", cartas) # mostra escrito Lista original e depois mostra a lista de cartas inicial
+plt.figure()
+x = range (0, 20)
+plt.plot(x, cartas, 'ok')
+plt.title ('range-x-cartas')
+plt.xlabel ('range')
+plt.ylabel ('cartas')
+plt.savefig ('bubble-inicio.png')
+plt.close ()
 N = 20 # numero de cartas
 for i in range(0, N - 1, 1): # i variando de 0 ate N - 1 exclusive de 1 em 1, sendo ate N - 1 porque o intervalo final do python e aberto
     for j in range(i + 1, N, 1): # j variando de i + 1 ate N exclusive de 1 em 1, sendo ate N porque o intervalo final do python e aberto
@@ -10,3 +19,10 @@ for i in range(0, N - 1, 1): # i variando de 0 ate N - 1 exclusive de 1 em 1, se
 print("Lista em ordem crescente", cartas) # mostra escrito Lista em ordem crescente e depois mostra a lista das cartas ordenadas em ordem crescente
 print ("Cinco menores valores", cartas[0: 5: 1])
 print ("cinco maiores valores", cartas[15: 20: 1])
+plt.figure()
+plt.plot(x, cartas, 'ok')
+plt.title ('range-x-cartas')
+plt.xlabel ('range')
+plt.ylabel ('cartas')
+plt.savefig ('bubble-fim.png')
+plt.close ()
