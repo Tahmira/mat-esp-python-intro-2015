d0b164f7269a1447d921f97f1c773c63a03777a7
Guilherme Malagutti <guimalacastro.gm@gmail.com>
2015-09-25 11:23:58 -0300

    colocamos o programa para rodar todas as trocas e salvar cada imagem

--- maria-guilherme-luiz/bubble-sort.py
+++ maria-guilherme-luiz/bubble-sort.py
@@ -9,13 +9,22 @@ plt.xlabel ('range')
 plt.ylabel ('cartas')
 plt.savefig ('fig/bubble-inicio.png')
 plt.close ()
+a = 0
 N = 20 # numero de cartas
 for i in range(0, N - 1, 1): # i variando de 0 ate N - 1 exclusive de 1 em 1, sendo ate N - 1 porque o intervalo final do python e aberto
     for j in range(i + 1, N, 1): # j variando de i + 1 ate N exclusive de 1 em 1, sendo ate N porque o intervalo final do python e aberto
-        if cartas[i] > cartas[j]: # se a carta [i] for maior que a carta[j]:
+            if cartas[i] > cartas[j]: # se a carta [i] for maior que a carta[j]:
             temp = cartas[i] #  a carta[i] em temp, para mover a carta[i] para um local temporario temp
             cartas[i] = cartas[j] # colocar a carta[j] em carta[i], para mover a carta[j] para a posicao da carta[i]
             cartas[j] = temp # colocar temp em carta[j], para mover a carta[i] que esta em temp para a posicao da carta[j]
+            plt.figure()
+            plt.plot(x, cartas, 'ok')
+            plt.title ('range-x-cartas')
+            plt.xlabel ('range')
+            plt.ylabel ('cartas')
+            a = a + 1
+            plt.savefig ('fig/bubble{}.png'.format(a))
+            plt.close()
 print("Lista em ordem crescente", cartas) # mostra escrito Lista em ordem crescente e depois mostra a lista das cartas ordenadas em ordem crescente
 print ("Cinco menores valores", cartas[0: 5: 1])
 print ("cinco maiores valores", cartas[15: 20: 1])
